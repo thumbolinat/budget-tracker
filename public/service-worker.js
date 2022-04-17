b8d6bd89666e8ccdceb77a61e0ac59116ae2f22b
thumbolinat <93739066+thumbolinat@users.noreply.github.com>
2022-04-16 22:14:18 -0400

    updated files

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,20 +4,20 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 
 // which items are we going to cache?
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/js/idb.js",
-    "/js/index.js",
-    "/css/styles.css",
-    "/icons/icon-512x512.png",
-    "/icons/icon-384x384.png",
-    "/icons/icon-192x192.png",
-    "/icons/icon-152x152.png",
-    "/icons/icon-144x144.png",
-    "/icons/icon-128x128.png",
-    "/icons/icon-96x96.png",
-    "/icons/icon-72x72.png"
-  ];
+  "./index.html",
+  "./css/styles.css",
+  "./js/index.js",
+  "./js/idb.js",
+  "./manifest.json",
+  "./icons/icon-512x512.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-72x72.png",
+]
 
 self.addEventListener('install', function (e) {
     //  tell the browser to wait until the work is complete before terminating the service worker
--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
-  
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 // routes
